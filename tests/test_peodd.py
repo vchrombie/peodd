cef95224031f50b469eb21240a73213f2bc872ed
Venu Vardhan Reddy Tekula <venu@chaoss.community>
2021-07-22 12:31:16 +0530

    [tests] Add some more tests covering the Exceptions
    Signed-off-by: Venu Vardhan Reddy Tekula <venu@chaoss.community>

--- tests/test_peodd.py
+++ tests/test_peodd.py
@@ -30,6 +30,8 @@ import click.testing
 from peodd._version import __version__
 from peodd import peodd
 
+from release_tools.repo import RepositoryError
+
 
 DEV_DEPENDENCIES_CONTENT_PYPROJECT_TOML = """[tool.poetry.dev-dependencies]
 foo = "^1.2.1"
@@ -37,19 +39,43 @@ bar = ">=1.2.2"
 baz = "1.2.3"
 """
 
+INVALID_CONTENT_PYPROJECT_TOML = """[tool.poetry.dev-dependencies
+foo = "^1.2.1"
+bar = ">=1.2.2"
+baz = "1.2.3"
+"""
+
+INVALID_VERSION_NUMBER_CONTENT = """[tool.poetry.dev-dependencies]
+foo = "^1.2.1"
+bar = "+1.2.2"
+baz = "1.2.3"
+"""
+
 DEV_DEPENDENCIES_CONTENT_REQUIREMENTS_TXT = (
     "foo>=1.2.1\nbar>=1.2.2\nbaz==1.2.3\n"
+)
+
+MOCK_REPOSITORY_ERROR = (
+    "Error: "
+)
+
+INVALID_PYPROJECT_TOML_ERROR = (
+    "Error: {}/pyproject.toml file is not valid"
+)
+
+INVALID_VERSION_NUMBER_ERROR = (
+    "Error: Please check the version number"
 )
 
 
 class TestPeodd(unittest.TestCase):
 
     @staticmethod
-    def setup_pyproject_file(filepath):
+    def setup_pyproject_file(filepath, content):
         """Set up some dev-dependencies in the pyproject file"""
 
         with open(filepath, mode='w') as fd:
-            fd.write(DEV_DEPENDENCIES_CONTENT_PYPROJECT_TOML)
+            fd.write(content)
 
     @unittest.mock.patch('peodd.peodd.Project')
     def test_peodd_script(self, mock_project):
@@ -63,7 +89,7 @@ class TestPeodd(unittest.TestCase):
         with runner.isolated_filesystem() as fs:
             project_file = os.path.join(fs, "pyproject.toml")
             mock_project.return_value.pyproject_file = project_file
-            self.setup_pyproject_file(project_file)
+            self.setup_pyproject_file(project_file, DEV_DEPENDENCIES_CONTENT_PYPROJECT_TOML)
 
             # Run the script command
             result = runner.invoke(peodd.main, ['-o', 'requirements-dev.txt'])
@@ -73,8 +99,73 @@ class TestPeodd(unittest.TestCase):
             with open(filepath, 'r') as fd:
                 text = fd.read()
 
-            self.assertEqual(text,
-                             DEV_DEPENDENCIES_CONTENT_REQUIREMENTS_TXT)
+            self.assertEqual(text, DEV_DEPENDENCIES_CONTENT_REQUIREMENTS_TXT)
+
+    @unittest.mock.patch('peodd.peodd.Project')
+    def test_repository_error(self, mock_project):
+        """Check if it stops working when it encounters RepositoryError exception"""
+
+        runner = click.testing.CliRunner(mix_stderr=False)
+
+        with runner.isolated_filesystem() as fs:
+
+            project_file = os.path.join(fs, "pyproject.toml")
+            mock_project.return_value.pyproject_file = project_file
+            self.setup_pyproject_file(project_file, DEV_DEPENDENCIES_CONTENT_PYPROJECT_TOML)
+
+            mock_project.side_effect = RepositoryError()
+
+            # Run the script command
+            result = runner.invoke(peodd.main, ['-o', 'requirements-dev.txt'])
+
+            self.assertEqual(result.exit_code, 1)
+
+            lines = result.stderr.split('\n')
+            self.assertEqual(lines[-2], MOCK_REPOSITORY_ERROR)
+
+            filepath = os.path.join(fs, 'requirements-dev.txt')
+            self.assertEqual(os.path.exists(filepath), False)
+
+    @unittest.mock.patch('peodd.peodd.Project')
+    def test_invalid_pyproject_toml(self, mock_project):
+        """Check if it stops working for an invalid pyproject.toml file"""
+
+        runner = click.testing.CliRunner(mix_stderr=False)
+
+        with runner.isolated_filesystem() as fs:
+            project_file = os.path.join(fs, "pyproject.toml")
+            mock_project.return_value.pyproject_file = project_file
+            self.setup_pyproject_file(project_file, INVALID_CONTENT_PYPROJECT_TOML)
+
+            # Run the script command
+            result = runner.invoke(peodd.main, ['-o', 'requirements-dev.txt'])
+            self.assertEqual(result.exit_code, 1)
+
+            lines = result.stderr.split('\n')
+            self.assertEqual(lines[-2], INVALID_PYPROJECT_TOML_ERROR.format(fs))
+
+            filepath = os.path.join(fs, 'requirements-dev.txt')
+            self.assertEqual(os.path.exists(filepath), False)
+
+    @unittest.mock.patch('peodd.peodd.Project')
+    def test_invalid_version_number(self, mock_project):
+        """Check if it stops working for an invalid version number for dev-dependencies"""
+
+        runner = click.testing.CliRunner(mix_stderr=False)
+
+        with runner.isolated_filesystem() as fs:
+            project_file = os.path.join(fs, "pyproject.toml")
+            mock_project.return_value.pyproject_file = project_file
+            self.setup_pyproject_file(project_file, INVALID_VERSION_NUMBER_CONTENT)
+
+            # Run the script command
+            result = runner.invoke(peodd.main, ['-o', 'requirements-dev.txt'])
+            self.assertEqual(result.exit_code, 1)
+
+            self.assertRaises(Exception)
+
+            lines = result.stderr.split('\n')
+            self.assertEqual(lines[-2], INVALID_VERSION_NUMBER_ERROR.format(fs))
 
     def test_version(self):
         self.assertRegex(__version__, peodd.VERSION_NUMBER_REGEX)
